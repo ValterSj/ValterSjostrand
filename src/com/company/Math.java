bcb398204c088192aa39e071ada929b11f3ff6da
Valter Sjöstrand <Valter.sjo@gmail.com>
2019-12-03 10:50:49 +0100

    Changed the sub method in class

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -5,7 +5,7 @@ public class Math {
     public int add(int fist, int second){
         return fist + second;
     }
-    public int sub(int first, int second){
-        return first - second;
+    public int sub(int first, int second, int forth){
+        return first - second + forth;
     }
 }
